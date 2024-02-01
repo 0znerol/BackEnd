ce43b8b3aad7f5c1f5af1e3f2c5193c3af79ad89
oznerol <lorenzo.pezzica98@gmail.com>
2024-02-01 16:53:22 +0100

    modified:   S1/L2/client/controller.php

--- S1/L2/client/controller.php
+++ S1/L2/client/controller.php
@@ -1,10 +1,10 @@
 <?php
         //var_dump($_SERVER);
         if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])){
-            if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-                echo "email non valida";
-                return;
-            }
+            // if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+            //     echo "email non valida";
+            //     return;
+            // }
             $passLen = strlen($_POST['password']);
             $safePass = $_POST['password'];
             for($i = 0; $i < $passLen; $i++){
@@ -21,8 +21,42 @@
 
             ];
             
-            session_write_close();
-            header('Location: http://localhost:4000/index.php');
+        }
+        var_dump($_FILES['file']);
+        if(isset($_FILES['file'])){
+            var_dump($_FILES['file']);
+            $file_name = $_FILES['file']["name"];
+            $file_type = $_FILES['file']["type"];
+            $file_size = $_FILES['file']["size"];
+            
+            $target_dir = "uploads/";
+    
+            if(!empty($_FILES['file'])) {
+                echo '<h3>File Name: ' . $file_name . '</h3>';
+                echo '<h3>File Type: ' . $file_type . '</h3>';
+                echo '<h3>File Size: ' . $file_size . '</h3>';
+                if($file_type === 'image/png') {
+                    if($file_size < 400000) {
+                        // is_uploaded_file è una funzione predefinita di PHP che controlla se un file è presente o meno
+                        if(is_uploaded_file($_FILES['file']["tmp_name"]) && $_FILES['file']["error"] === UPLOAD_ERR_OK) {
+                            // move_uploaded_file è una funzione predefinita di PHP che mi permette di spostare un file 
+                            // caricato nella cartella temporanea in una cartella definitiva
+                            if(move_uploaded_file($_FILES['file']["tmp_name"], $target_dir.$file_name)) {
+                                echo 'Caricamento avvenuto con successo';
+                                $_SESSION['safeLogin']['img'] = $file_name;
+                                header('Location: http://localhost:4000/index.php');
+
+                            } else {
+                                echo 'Errore!!!';
+                            }
+                        }
+                    } else {
+                        echo 'FileSize troppo grande';
+                    }
+                } else {
+                    echo 'FileType non supportato';
+                }
+            }
         }
 
     ?>
--- S1/L2/client/index.php
+++ S1/L2/client/index.php
@@ -7,21 +7,25 @@
     <title>Document</title>
   </head>
   <body class='row'>
-    <form class='row w-75 m-auto' action="controller.php" method="POST">
+    <form class='row w-75 m-auto' action="controller.php" method="POST" enctype="multipart/form-data">
       <label for="email">Email:</label>
       <input type="text" id="email" name="email">
       <label for="name">Name:</label>
       <input type="text" id="name" name="name">
       <label for="password">Password:</label>
       <input type="password" id="password" name="password"></input>
-      <input class="w-25 mt-2" type="submit" value="Submit">
+  
+      <input type="file" name="file" id="file">
+      <input class="w-25 mt-2" type="submit" value="Submit"></input>
     </form>
 
     <div class="row mt-5">
+      
       <?php
         session_start();
         if($_SESSION['safeLogin']){
           echo '<h1>logged as</h1>';
+          echo "<img class='w-25 h-25' src='uploads/".$_SESSION['safeLogin']['img']."' alt=''>";
           echo "<h3>"."Email: " . $_SESSION['safeLogin']['email'] ."</h3>"."<h3>"."Name: " . $_SESSION['safeLogin']['name'] ."</h3>". "<br>" ."<h4>". "password: " . $_SESSION['safeLogin']['password'] ."</h4>";
           echo '<form action="logout.php" method="POST">
                   <input type="submit" value="logout">
