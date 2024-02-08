fa4134970a7a66080019765f1c1e1e9a39c67639
oznerol <lorenzo.pezzica98@gmail.com>
2024-02-08 09:38:37 +0100

    modified:   S1/PS/navbar.php

--- S1/PS/navbar.php
+++ S1/PS/navbar.php
@@ -14,18 +14,6 @@
           <li class="nav-item">
             <a class="nav-link" href="add_book_form.php">Add book</a>
           </li>
-          <!-- <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Genere
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Fantasy</a></li>
-              <li><a class="dropdown-item" href="#">Sci-fi</a></li>
-              <li><a class="dropdown-item" href="#">Horror</a></li>
-              <li><a class="dropdown-item" href="#">Other</a></li>
-
-            </ul>
-          </li> -->
         </ul>
         <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
