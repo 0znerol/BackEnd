d6ed7a01d43324c95fdb37e49db400e800c930c1
0znerol <lorenzo.pezzica98@gmail.com>
2024-03-06 16:13:07 +0100

    modified:   S5/Progetto_Settimanale/login.php

--- S5/Progetto_Settimanale/login.php
+++ S5/Progetto_Settimanale/login.php
@@ -14,22 +14,22 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
-<body class="d-flex flex-column align-items-center">
-    <h2>Login</h2>
+<body class="d-flex flex-column align-items-center bg-dark">
+    <h2 class="text-light">Login</h2>
     <?php if (isset($error)) { ?>
         <p><?php echo $error; ?></p>
     <?php } ?>
     <form action="log_auth.php" method="post">
         <input type="text" name="username" placeholder="Username" required><br>
         <input type="password" name="pass" placeholder="Password" required><br>
-        <button type="submit">Login</button>
+        <button type="submit" class="btn border text-light">Login</button>
     </form>
     <h2>Register</h2>
     <form action="register.php" method="post">
         <input type="text" name="username" placeholder="Username" required><br>
         <input type="email" name="email" placeholder="Email" required><br>
         <input type="password" name="pass" placeholder="Password" required><br>
-        <button type="submit">Register</button>
+        <button type="submit" class="btn border text-light">Register</button>
     </form>
     <?php if (isset($_GET['error'])) { 
                 // print_r($_SESSION['error']);
